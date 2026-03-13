5cde2bb87a1ba89ccba2d5ae5bcb6f5e690ff6c7
sami-403 <francisco.samily1@gmail.com>
2026-03-13 13:57:49 -0300

    Fix password comment in sequelizeFoods configuration

--- database/db.js
+++ database/db.js
@@ -17,7 +17,7 @@ export const sequelizeFoods = new Sequelize({
   dialect: PostgresDialect,
   database: process.env.DB_FOODS_NAME,
   user: process.env.DB_FOODS_USER,
-  password: process.env.DB_FOODS_PASS, // <--- VEJA SE NÃO ESTÁ DB_USERS_PASS AQUI
+  password: process.env.DB_FOODS_PASS, 
   host: process.env.DB_FOODS_HOST,
   port: Number(process.env.DB_FOODS_PORT),
 });
